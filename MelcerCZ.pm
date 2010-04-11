77dd66385c71ade215228254ada2326821f5e2a5
Michal Špaček <skim@cpan.org>
2010-04-11 11:07:38 +0000

    Encode to right utf8 encoding. Remove whitespace.

--- MelcerCZ.pm
+++ MelcerCZ.pm
@@ -8,8 +8,10 @@ use strict;
 use warnings;
 
 # Modules.
+use Encode qw(decode_utf8 encode_utf8);
 use LWP::UserAgent;
 use Readonly;
+use Text::Iconv;
 use Web::Scraper;
 use WWW::Search qw(generic_option);
 
@@ -24,12 +26,14 @@ our $VERSION = 0.01;
 #------------------------------------------------------------------------------
 sub native_setup_search {
 #------------------------------------------------------------------------------
-# TODO
+# Setup.
 
 	my ($self, $query) = @_;
-	$self->{'_offset'} = 0;
-	$self->{'_query'} = $query;
 	$self->{'_def'} = scraper {
+		process '//meta[@http-equiv="Content-Type"]', 'encoding' => [
+			'@content',
+			\&_get_encoding,
+		];
 		process '//td[@height="330"]/node()[3]', 'records' => 'RAW';
 		process '//td[@height="330"]/table[@width="560"]', 'books[]'
 			=> scraper {
@@ -46,13 +50,15 @@ sub native_setup_search {
 		};
 		return;
 	};
+	$self->{'_offset'} = 0;
+	$self->{'_query'} = $query;
 	return 1;
 }
 
 #------------------------------------------------------------------------------
 sub native_retrieve_some {
 #------------------------------------------------------------------------------
-# TODO
+# Get data.
 
 	my $self = shift;
 
@@ -72,15 +78,85 @@ sub native_retrieve_some {
 	# Process.
 	if ($response->is_success) {
 		my $content = $response->content;
-		my $book_hr = $self->{'_def'}->scrape($content);
-		foreach my $book_hr (@{$book_hr->{'books'}}) {
+
+		# Get books structure.
+		my $books_hr = $self->{'_def'}->scrape($content);
+
+	
+		# Iconv.
+		if (! $self->{'_iconv'} && $books_hr->{'encoding'}) {
+			$self->{'_iconv'} = Text::Iconv->new(
+				$books_hr->{'encoding'}, 'utf-8');
+		}
+
+		# Process each book.
+		foreach my $book_hr (@{$books_hr->{'books'}}) {
 			if (exists $book_hr->{'url'}) {
 				$book_hr->{'url'} = $MELCER_CZ.
 					$book_hr->{'url'};
 			}
-			push @{$self->{'cache'}}, $book_hr;
+			push @{$self->{'cache'}}, $self->_process($book_hr);
 		}
 	}
+
+	return;
+}
+
+#------------------------------------------------------------------------------
+# Private subroutines and methods.
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+sub _get_encoding {
+#------------------------------------------------------------------------------
+# Get enconding from Content-Type string.
+
+	my $content_type = shift;
+	if ($content_type =~ m/.*charset=(.*)$/ms) {
+		return $1;
+	} else {
+		return;
+	}
+}
+
+#------------------------------------------------------------------------------
+sub _process {
+#------------------------------------------------------------------------------
+# Process each parameter of structure.
+
+	my ($self, $book_hr) = @_;
+	$self->_process_one($book_hr, 'author');
+	$self->_process_one($book_hr, 'info');
+	$self->_process_one($book_hr, 'publisher');
+	$self->_process_one($book_hr, 'title');
+	return $book_hr;
+}
+
+#------------------------------------------------------------------------------
+sub _process_one {
+#------------------------------------------------------------------------------
+# Process string to right output:
+# - Encode to utf8.
+# - Remove trailing whitespace.
+
+
+	my ($self, $book_hr, $key) = @_;
+
+	# Encode to utf8.
+	if ($self->{'_iconv'} && exists $book_hr->{$key}) {
+		$book_hr->{$key} = $self->{'_iconv'}->convert(
+			$book_hr->{$key});
+	}
+
+	# Encode to perl internal form.
+	$book_hr->{$key} = decode_utf8($book_hr->{$key});
+
+	# Remove trailing whitespace.
+	$book_hr->{$key} =~ s/^\s+//gms;
+	$book_hr->{$key} =~ s/\s+$//gms;
+
+	# Encode to utf8 for output.
+	$book_hr->{$key} = encode_utf8($book_hr->{$key});
 
 	return;
 }
